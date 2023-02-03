29579a630aeb0ee036de2fc1353c0634a8c658e8
Andy Zheng <97316979+Andrew-bro2022@users.noreply.github.com>
2023-02-02 23:14:10 -0500

    Update Lib.h

--- Lib.h
+++ Lib.h
@@ -1,15 +1,7 @@
 /*
 *****************************************************************************
-                    Milestone #5
+File: Lib.h
 Full Name  : Yuchi Zheng
-Student ID#: 025 848 151
-Email      : yzheng103@myseneca.ca
-Section    : NAA
-Date       : 7, August, 2022
-
-Authenticity Declaration:
-I have done all the coding by myself and only copied the code
-that my professor provided to complete my workshops and assignments.
 *****************************************************************************
 */
 
