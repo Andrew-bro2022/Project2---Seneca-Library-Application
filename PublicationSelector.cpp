83d4329162e1a4eba1b871133fa2d47f2094a69f
Andy Zheng <97316979+Andrew-bro2022@users.noreply.github.com>
2023-02-02 23:18:47 -0500

    Update PublicationSelector.cpp

--- PublicationSelector.cpp
+++ PublicationSelector.cpp
@@ -1,27 +1,6 @@
-// Final Project 
 // PublicationSelector
 // File PublicationSelector.h
-// Version 1.0
-// Author	Fardad Soleimanloo
-// Revision History
-// -----------------------------------------------------------
-// Name               Date                 Reason
-/////////////////////////////////////////////////////////////////
-
-/*
-*****************************************************************************
-                    Milestone #5
-Full Name  : Yuchi Zheng
-Student ID#: 025 848 151
-Email      : yzheng103@myseneca.ca
-Section    : NAA
-Date       : 7, August, 2022
-
-Authenticity Declaration:
-I have done all the coding by myself and only copied the code
-that my professor provided to complete my workshops and assignments.
-*****************************************************************************
-*/
+// Full Name  : Yuchi Zheng
 
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
