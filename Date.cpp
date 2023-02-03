f6c877975ed3e72607a6afba01a1d0a1bf69362a
Andy Zheng <97316979+Andrew-bro2022@users.noreply.github.com>
2023-02-02 23:12:39 -0500

    Update Date.cpp

--- Date.cpp
+++ Date.cpp
@@ -1,27 +1,8 @@
-// Final Project Milestone 5
+
 // Date Module
 // File  Date.cpp
-// Version 1.0
-// Author   Fardad Soleimanloo
-// Revision History
-// -----------------------------------------------------------
-// Name               Date                 Reason
-/////////////////////////////////////////////////////////////////
+// Full Name  : Yuchi Zheng
 
-/*
-*****************************************************************************
-                    Milestone #5
-Full Name  : Yuchi Zheng
-Student ID#: 025 848 151
-Email      : yzheng103@myseneca.ca
-Section    : NAA
-Date       : 7, August, 2022
-
-Authenticity Declaration:
-I have done all the coding by myself and only copied the code
-that my professor provided to complete my workshops and assignments.
-*****************************************************************************
-*/
 
 #define _CRT_SECURE_NO_WARNINGS
 #include <iomanip>
